8f060201731e62eff75e3be7887b4a0e2231a4cd
ElTitoDG <julianhinojosagil@gmail.com>
2024-03-01 12:28:21 +0100

    setup for html file, but doesn't work

--- webserver.c
+++ webserver.c
@@ -1,12 +1,13 @@
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-
 #define PORT 8080
 #define BUFFER_SIZE 1024
+#define MAX_FILE_SIZE 209715200 // 200Mb
 
 int main()
 {
@@ -17,6 +18,15 @@ int main()
                 "<html>hello, world</html>\r\n"
                 "<html>From C WebServer</html>\r\n";
 
+
+
+  char *file_content = malloc(MAX_FILE_SIZE);
+  if (!file_content)
+  {
+      perror("webserver (malloc)");
+      return 1;
+  }
+
   // Creamos el socket || sockfd == socket file descriptor
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd == -1)
@@ -24,7 +34,7 @@ int main()
     perror("webserver (socket)");
     return 1;
   }
-  printf("Socket created successfully\n");
+  printf("Socket creado con éxito\n");
 
   // Creamos la dirección para vincularla al socket
   struct sockaddr_in host_addr;
@@ -44,7 +54,7 @@ int main()
     perror("webserver (bind)");
     return 1;
   }
-  printf("Socket successfully bound to address\n");
+  printf("Socket enlazado con éxito a la dirección\n");
 
   // Escuchando conexiones entrantes
   if (listen(sockfd, SOMAXCONN) != 0)
@@ -52,10 +62,11 @@ int main()
     perror("webserver (listen)");
     return 1;
   }
-  printf("Server listening for connections\n");
+  printf("Servidor a la espera de conexiones\n");
 
   // Bucle infinito para aceptar conexiones
   for (;;) {
+
     // Aceptamos conexiones entrantes
     int newsockfd = accept(sockfd, (struct sockaddr *)&host_addr,
                            (socklen_t *)&host_addrlen);
@@ -63,9 +74,9 @@ int main()
     if (newsockfd < 0)
     {
       perror("webserver (accept)");
-      return 1;
+      continue;
     }
-    printf("Connection accepted\n");
+    printf("Conexión aceptada\n");
 
     // Sacamos la direccion del cliente
     int sockn = getsockname(newsockfd, (struct sockaddr *)&client_addr,
@@ -87,8 +98,47 @@ int main()
     // Leemos la peticion
     char method[BUFFER_SIZE], uri[BUFFER_SIZE], version[BUFFER_SIZE];
     sscanf(buffer, "%s %s %s", method, uri, version);
-    printf("[%s:%u] %s %s %s\n", inet_ntoa(client_addr.sin_addr),
+    printf("[%s:%u]\n Metodo: %s\n URI: %s\n Version: %s\n", inet_ntoa(client_addr.sin_addr),
            ntohs(client_addr.sin_port), method, uri, version);
+
+    if (strcmp(uri, "/") == 0)
+    {
+        FILE *file = fopen("index.html", "r");
+        if (file == NULL)
+        {
+            perror("webserver (fopen)");
+            continue;
+        }
+
+        //Obtenemos el tamaño del archivo
+        fseek(file, 0, SEEK_END);
+        long file_size = ftell(file);
+        fseek(file, 0, SEEK_SET);
+
+        // Comprobar si el tamaño del archivo es mayor que MAX_FILE_SIZE
+        if (file_size > MAX_FILE_SIZE)
+        {
+            fprintf(stderr, "Error: index.html es demasiado grande\n");
+            fclose(file);
+            continue;
+        }
+
+        // Leer el archivo en el buffer
+        size_t nread = fread(file_content, 1, file_size, file);
+        if (nread != file_size)
+        {
+            perror("webserver (fread)");
+            continue;
+        }
+        fclose(file);
+    }
+
+    //Respuesta al cliente
+    sprintf(resp, "HTTP/1.0 200 OK\r\n"
+                    "Server: webserver-c\r\n"
+                    "Content-type: text/html\r\n"
+                    "Content-Length: %lu\r\n\r\n"
+                    "%s", strlen(file_content), file_content);
 
     // Escribir en el socket
     int valwrite = write(newsockfd, resp, strlen(resp));
@@ -100,6 +150,8 @@ int main()
 
     close(newsockfd);
   }
+
+  free(file_content);
 
   return 0;
 }
