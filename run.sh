ea12dc2621b2a3c5e8ca4d5f0564581f4e7d5051
ElTitoDG <julianhinojosagil@gmail.com>
2024-03-01 14:16:33 +0100

    added conditional for creating build dir

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
+# Ruta del directorio a comprobar y crear si no existe
+DIRECTORIO="build"
+
+# Comprobar si el directorio existe
+if [ ! -d "$DIRECTORIO" ]; then
+  # Crear el directorio si no existe
+  mkdir -p "$DIRECTORIO"
+fi
+
+
 cd build
 
 cmake ..
